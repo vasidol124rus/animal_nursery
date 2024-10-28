12a568d1f5605258b06a27e70fb6f055e522696c
Vasiliy Kuznetsov <vasekku25@gmail.com>
2024-10-28 20:17:25 +0700

    создал Animal

---
+++ src/animals_models/Animal.java
@@ -0,0 +1,48 @@
+package animals_models;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Animal {
+    private String name;
+    private String type;
+    private LocalDate birthdate;
+    private List<String> commands = new ArrayList<>();
+
+    public Animal(String name, String type, LocalDate birthdate) {
+        this.name = name;
+        this.type = type;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public List<String> getCommands() {
+        return commands;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", birthdate=" + birthdate +
+                ", commands=" + commands +
+                '}';
+    }
+    public void learnCommand(String command) {
+        commands.add(command);
+    }
+}
+
